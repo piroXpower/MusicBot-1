21cfe04b1d980a02b98d36f2ec9a1df2970a8aba
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 18:46:43 +0530

    Update admin.py

--- RaichuX/modules/admin.py
+++ RaichuX/modules/admin.py
@@ -1,8 +1,8 @@
 
 from asyncio import QueueEmpty
 
-from RaichuX.helpers.Calls import tgcalls
-from RaichuX.helpers.Queues.queues import queues
+from Image import tgcalls
+from Image.queues import queues
 from RaichuX import BOT_USERNAME, que
 from RaichuX.helpers.Functions.admins import admins
 from RaichuX.helpers.debug.channelmusic import get_chat_id
@@ -54,12 +54,12 @@ async def update_admin(client, message):
 @authorized_users_only
 async def pause(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    for x in tgcalls.pytgcalls.active_calls:
+    for x in Image.pyImage.active_calls:
         ACTV_CALLS(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
         await message.reply_text("❌ **no music is currently playing**")
     else:
-        await tgcalls.pytgcalls.pause_stream(chat_id)
+        await Image.pyImage.pause_stream(chat_id)
         await message.reply_text(
             "⏸ **Track paused.**\n\n• **To resume the playback, use the**\n» /resume command."
         )
@@ -70,12 +70,12 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    for x in tgcalls.pytgcalls.active_calls:
+    for x in Image.pyImage.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
         await message.reply_text("❌ **no music is paused**")
     else:
-        await tgcalls.pytgcalls.resume_stream(chat_id)
+        await Image.pyImage.resume_stream(chat_id)
         await message.reply_text(
             "▶️ **Track resumed.**\n\n• **To pause the playback, use the**\n» /pause command."
         )
@@ -86,7 +86,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    for x in tgcalls.pytgcalls.active_calls:
+    for x in Image.pyImage.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
         await message.reply_text("❌ **no music is currently playing**")
@@ -95,7 +95,7 @@ async def stop(_, message: Message):
             queues.clear(chat_id)
         except QueueEmpty:
             pass
-        await tgcalls.pytgcalls.leave_group_call(chat_id)
+        await Image.pyImage.leave_group_call(chat_id)
         await message.reply_text("✅ **music playback has ended**")
 
 
@@ -105,7 +105,7 @@ async def stop(_, message: Message):
 async def skip(_, message: Message):
     global que
     chat_id = message.chat.id
-    for x in tgcalls.pytgcalls.active_calls:
+    for x in Image.pyImage.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
         await message.reply_text("❌ **no music is currently playing**")
@@ -113,16 +113,16 @@ async def skip(_, message: Message):
         queues.task_done(chat_id)
         
         if queues.is_empty(chat_id):
-            await tgcalls.pytgcalls.leave_group_call(chat_id)
+            await Image.pyImage.leave_group_call(chat_id)
         else:
-            await tgcalls.pytgcalls.change_stream(
-            chat_id, 
-            InputStream(
-                InputAudioStream(
-                    queues.get(chat_id)["file"],
+            await Image.pyImage.change_stream(
+                chat_id, 
+                InputStream(
+                    InputAudioStream(
+                        Image.queues.get(chat_id)["file"],
+                    ),
                 ),
-            ),
-        )
+            )
                 
     qeue = que.get(chat_id)
     if qeue:
@@ -130,53 +130,3 @@ async def skip(_, message: Message):
     if not qeue:
         return
     await message.reply_text("⏭ **You've skipped to the next song.**")
-
-
-@Client.on_message(command(["auth", f"auth@{BOT_USERNAME}"]) & other_filters)
-@authorized_users_only
-async def authenticate(client, message):
-    global admins
-    if not message.reply_to_message:
-        return await message.reply("💡 reply to message to authorize user !")
-    if message.reply_to_message.from_user.id not in admins[message.chat.id]:
-        new_admins = admins[message.chat.id]
-        new_admins.append(message.reply_to_message.from_user.id)
-        admins[message.chat.id] = new_admins
-        await message.reply(
-            "🟢 user authorized.\n\nfrom now on, that's user can use the admin commands."
-        )
-    else:
-        await message.reply("✅ user already authorized!")
-
-
-@Client.on_message(command(["unauth", f"deauth@{BOT_USERNAME}"]) & other_filters)
-@authorized_users_only
-async def deautenticate(client, message):
-    global admins
-    if not message.reply_to_message:
-        return await message.reply("💡 reply to message to deauthorize user !")
-    if message.reply_to_message.from_user.id in admins[message.chat.id]:
-        new_admins = admins[message.chat.id]
-        new_admins.remove(message.reply_to_message.from_user.id)
-        admins[message.chat.id] = new_admins
-        await message.reply(
-            "🔴 user deauthorized.\n\nfrom now that's user can't use the admin commands."
-        )
-    else:
-        await message.reply("✅ user already deauthorized!")
-
-
-
-
-# For Volume Control
-
-@Client.on_message(command(["volume", f"volume@{BOT_USERNAME}"]) & other_filters)
-@authorized_users_only
-async def change_volume(client, message):
-    range = message.command[1]
-    chat_id = message.chat.id
-    try:
-       await tgcalls.pytgcalls.change_volume_call(chat_id, volume=int(range))
-       await message.reply(f"✅ **volume set to:** ```{range}%```")
-    except Exception as e:
-       await message.reply(f"**error:** {e}")

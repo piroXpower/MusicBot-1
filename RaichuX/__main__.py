03f57fd7ac49b06c352ec154c4e42756f4728c77
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 11:04:33 +0530

    Update __main__.py

--- RaichuX/__main__.py
+++ RaichuX/__main__.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import Client as Bot
 from RaichuX.helpers.Calls.tgcalls import run
 from pytgcalls import idle
-from RaichuX import API_ID, API_HASH, BOT_TOKEN
+from RaichuX import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
 response = requests.get(BG_IMAGE)
 with open("./Image/foreground.png", "wb") as file:

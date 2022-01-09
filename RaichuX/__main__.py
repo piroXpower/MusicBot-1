663348797c3f57fb95aafb660eb01ffa4a810a4d
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 09:11:15 +0530

    Update __main__.py

--- RaichuX/__main__.py
+++ RaichuX/__main__.py
@@ -2,7 +2,17 @@ from XRaichu.helpers.Calls.tgcalls import run
 from pyrogram import idle
 from RaichuX import API_ID, API_HASH, BOT_TOKEN
 
+with open("./Image/foreground.png", "wb") as file:
+    file.write(response.content)
 
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="RaixhuX.modules"),
+)
 
 
 

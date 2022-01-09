09928554a785296f347e511be079467059e8ef4c
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 09:02:08 +0530

    Update __init__.py

--- RaichuX/__init__.py
+++ RaichuX/__init__.py
@@ -1,1 +1,24 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+OWNER_ID = int(getenv("OWNER_ID"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+
+
+# EXTRA 
+
 

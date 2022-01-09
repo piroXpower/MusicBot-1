100b5079f5145e0aea01515e928573c80fdf398d
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 09:05:54 +0530

    Update __init__.py

--- RaichuX/__init__.py
+++ RaichuX/__init__.py
@@ -21,4 +21,9 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
 # EXTRA 
 
-
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/8628c642a266a22effd8c.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")

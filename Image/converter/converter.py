11997aa9604bac2970ae7f23565d5bdf5dc7fbb7
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-09 12:08:03 +0530

    Update converter.py

--- Image/converter/converter.py
+++ Image/converter/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from helpers.errors import FFmpegReturnCodeError
+from RaichuX.helpers.debug.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:

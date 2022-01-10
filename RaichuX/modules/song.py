3fe549738e00ae7a534130bfeb37f28028e41e59
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-10 21:06:56 +0530

    Update song.py

--- RaichuX/modules/song.py
+++ RaichuX/modules/song.py
@@ -1,7 +1,8 @@
-# Copyright (C) 2021 By VeezMusicProject
+# Copyright (C) 2021 By RaichuNews
 
 from __future__ import unicode_literals
 
+import re
 import asyncio
 import math
 import os
@@ -14,6 +15,8 @@ import aiohttp
 import requests
 import wget
 import yt_dlp
+import lyricsgenius
+from youtubesearchpython import VideosSearch
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
@@ -279,3 +282,58 @@ async def lyrics(_, message):
         await rep.edit(result)
     except Exception:
         await rep.edit("❌ **lyrics not found.**\n\n» **please give a valid song name.**")
+
+
+@Client.on_callback_query(filters.regex(pattern=r"lyrics"))
+async def lyricssex(_, CallbackQuery):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    try:
+        id, user_id = callback_request.split("|")
+    except Exception as e:
+        return await CallbackQuery.message.edit(
+            f"Error Occured\n**Possible reason could be**:{e}"
+        )
+    url = f"https://www.youtube.com/watch?v={id}"
+    print(url)
+    try:
+        results = VideosSearch(url, limit=1)
+        for result in results.result()["result"]:
+            title = result["title"]
+    except Exception as e:
+        return await CallbackQuery.answer(
+            "Sound not found. Youtube issues.", show_alert=True
+        )
+    x = "OXaVabSRKQLqwpiYOn-E4Y7k3wj-TNdL5RfDPXlnXhCErbcqVvdCF-WnMR5TBctI"
+    y = lyricsgenius.Genius(x)
+    t = re.sub(r"[^\w]", " ", title)
+    y.verbose = False
+    S = y.search_song(t, get_full_info=False)
+    if S is None:
+        return await CallbackQuery.answer(
+            "Lyrics not found :p", show_alert=True
+        )
+    await CallbackQuery.message.delete()
+    userid = CallbackQuery.from_user.id
+    usr = f"[{CallbackQuery.from_user.first_name}](tg://user?id={userid})"
+    xxx = f"""
+**Lyrics Search Powered By {bn}**
+**Searched By:-** {usr}
+**Searched Song:-** __{title}__
+**Found Lyrics For:-** __{S.title}__
+**Artist:-** {S.artist}
+**__Lyrics:__**
+{S.lyrics}"""
+    if len(xxx) > 4096:
+        filename = "lyrics.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(xxx.strip()))
+        await CallbackQuery.message.reply_document(
+            document=filename,
+            caption=f"**OUTPUT:**\n\n`Lyrics`",
+            quote=False,
+        )
+        os.remove(filename)
+    else:
+        await CallbackQuery.message.reply_text(xxx)
+
